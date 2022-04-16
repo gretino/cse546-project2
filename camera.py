844878a2f8d2d1073879e6711cc4f3172fdc295e
QiangFu <fuqiqiqiang@gmail.com>
2022-04-16 14:49:28 -0700

    bugfix

--- camera.py
+++ camera.py
@@ -1,7 +1,10 @@
 from picamera import PiCamera
 from time import sleep
 import datetime as dt
+import os
 import boto3
+from dotenv import load_dotenv
+load_dotenv()
 
 s3 = boto3.client('s3')
 def camera_record():
@@ -13,7 +16,7 @@ def camera_record():
 	sleep(0.5)
 	camera.stop_recording()
 	camera.stop_preview()
-
+    camera.close()
 	s3.upload_file(filepath,'input-project2',filename)
 
 while True:
