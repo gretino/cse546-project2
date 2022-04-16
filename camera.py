df883906da16b3cc7b922f16207a6585d49c96d9
KampallySreshtaChowdary <43363158+KampallySreshtaChowdary@users.noreply.github.com>
2022-04-16 15:15:00 -0600

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,21 +3,18 @@ from time import sleep
 import datetime as dt
 import boto3
 
-
-def upload_to_aws_s3(local_file, bucket, s3_file):
-    s3 = boto3.client('s3')
-    s3.upload_file('D:/cc2images/image.png','input-project2','filename')
-
+s3 = boto3.client('s3')
 def camera_record():
 	camera = PiCamera()
 	filename = dt.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+	filepath = f'/home/pi/Desktop/{filename}'
 	camera.start_preview()
-	camera.start_recording('/home/pi/Desktop/filename')
+	camera.start_recording(filepath)
 	sleep(0.5)
 	camera.stop_recording()
 	camera.stop_preview()
 
-	s3_upload.upload_to_aws_s3('/home/pi/Desktop/filename','input-project2','filename')
+	s3.upload_file(filepath,'input-project2',filename)
 
 while True:
 	camera_record()
