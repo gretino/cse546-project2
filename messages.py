cc0f30b41e57021a17f2b3993b028c3051482056
KampallySreshtaChowdary <43363158+KampallySreshtaChowdary@users.noreply.github.com>
2022-04-24 15:15:42 -0700

    Add files via upload

---
+++ messages.py
@@ -0,0 +1,39 @@
+import boto3
+import time
+import datetime as dt
+import json
+
+sqs_client = boto3.client("sqs", region_name="us-east-1")
+
+def delete_message(receipt_handle):
+    #print(receipt_handle)
+    response = sqs_client.delete_message(
+        QueueUrl="https://sqs.us-east-1.amazonaws.com/053622899218/project2_queue",
+        ReceiptHandle=receipt_handle,
+    )
+
+def receive_message():
+	message_body=''
+	response = sqs_client.receive_message(
+		QueueUrl='https://sqs.us-east-1.amazonaws.com/053622899218/project2_queue',
+		MaxNumberOfMessages=1,
+		WaitTimeSeconds=2,)
+	for message in response.get("Messages", []):
+		message_body = message["Body"]
+		delete_message(message["ReceiptHandle"])
+	return message_body
+
+print("Receiving SQS messages...")
+while True:
+	json_data = receive_message()
+	if not json_data == '':
+		#print(json_data)
+		y = json.loads(json_data)
+		print(f'The person detected at {y["filename"]}: {y["name"]}, {y["major"]}, {y["year"]}')
+		start_time = y["filename"][:-5]
+		print(f'start_time: {start_time}')
+		time_format = time.strptime(start_time, '%Y-%m-%d_%H.%M.%S.%f')
+		#print(time_format)
+		print(f'Latency: {time.time()-time.mktime(time_format)} seconds')
+	else:
+		print("Empty Queue...")
