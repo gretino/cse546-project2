9aae68c69e2892b3ff12d13cb23407a3c2d1740e
KampallySreshtaChowdary <43363158+KampallySreshtaChowdary@users.noreply.github.com>
2022-04-30 15:14:47 -0600

    Update messages.py

--- messages.py
+++ messages.py
@@ -2,10 +2,8 @@ import boto3
 import time
 import datetime as dt
 import json
-from dotenv import load_dotenv
-load_dotenv()
 
-
+i=0
 sqs_client = boto3.client("sqs", region_name="us-east-1")
 
 def delete_message(receipt_handle):
@@ -23,7 +21,7 @@ def receive_message():
 		WaitTimeSeconds=2,)
 	for message in response.get("Messages", []):
 		message_body = message["Body"]
-		# delete_message(message["ReceiptHandle"])
+		delete_message(message["ReceiptHandle"])
 	return message_body
 
 print("Receiving SQS messages...")
@@ -31,16 +29,17 @@ while True:
 	json_data = receive_message()
 	if not json_data == '':
 		#print(json_data)
+		i=i+1
 		y = json.loads(json_data)
-		print(f'The person detected at {y["filename"]}: {y["name"]}, {y["major"]}, {y["year"]}')
+		print(f'The {i} person detected: {y["name"]}, {y["major"]}, {y["year"]}')
 		start_time = y["filename"][:-5]
-		print(f'start_time: {start_time}, {type(start_time)}')
+		#print(f'start_time: {start_time}, {type(start_time)}')
 		time_format = time.strptime(start_time, '%Y-%m-%d_%H.%M.%S.%f')
 		millis = float(f"0.{start_time.split('.')[-1]}")
 		start_timestamp = time.mktime(time_format)+millis
 		current_timestamp = time.time()
-		print(start_time, start_timestamp, current_timestamp)
+		#print(start_time, start_timestamp, current_timestamp)
 		print(f'Latency: {"{:.2f}".format(current_timestamp-start_timestamp)} seconds.')
-		print('\n')
+		#print('\n')
 	else:
 		print("Empty Queue...")
