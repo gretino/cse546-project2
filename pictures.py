b1bc1fde1fcc03d3d9a2bdea4934d8affbe2026e
gretino <fuqiqiqiang@gmail.com>
2022-05-03 13:48:55 -0700

    Update pictures.py

--- pictures.py
+++ pictures.py
@@ -2,20 +2,22 @@ from picamera import PiCamera
 from time import sleep
 import datetime as dt
 
+name = input('Enter your name: ')
 def camera_record():
-	camera = PiCamera()
-	camera.resolution = (160, 160)
-	filename = dt.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
-	filepath = f'/home/pi/Desktop/{filename}'
-	camera.start_preview()
-	sleep(2)
-	camera.capture(filepath)
-	camera.stop_preview()
-	camera.close()
+    camera = PiCamera()
+    camera.resolution = (160, 160)
+    filename = dt.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
+    filepath = f'/home/pi/Desktop/{name}/{filename}'
+    camera.start_preview()
+    sleep(1)
+    camera.capture(filepath)
+    camera.stop_preview()
+    camera.close()
+
 
 while True:
-	x=input()
-	if (x == ''):
-		camera_record()
-	else: 
-		break
+    x = input()
+    if (x == ''):
+        camera_record()
+    else:
+        break
