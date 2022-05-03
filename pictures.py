4e404b6c01258686055f6d5dc28eab2dcc47797a
gretino <fuqiqiqiang@gmail.com>
2022-05-03 13:51:28 -0700

    Update pictures.py

--- pictures.py
+++ pictures.py
@@ -1,12 +1,19 @@
 from picamera import PiCamera
 from time import sleep
 import datetime as dt
+import os
 
 name = input('Enter your name: ')
+folder_path = f'/home/pi/Desktop/{name}/'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+
 def camera_record():
     camera = PiCamera()
     camera.resolution = (160, 160)
     filename = dt.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
+
     filepath = f'/home/pi/Desktop/{name}/{filename}'
     camera.start_preview()
     sleep(1)
