6f7cb0fb982f3c802c5f0e76f86829ec4c4884dd
gretino <fuqiqiqiang@gmail.com>
2022-05-04 16:28:19 -0700

    autoformatting, changed the time

--- messages.py
+++ messages.py
@@ -3,44 +3,48 @@ import time
 import datetime as dt
 import json
 from dotenv import load_dotenv
+
 load_dotenv()
-i=0
+i = 0
 sqs_client = boto3.client("sqs", region_name="us-east-1")
 
+
 def delete_message(receipt_handle):
-    #print(receipt_handle)
+    # print(receipt_handle)
     response = sqs_client.delete_message(
         QueueUrl="https://sqs.us-east-1.amazonaws.com/250982311839/project2_queue",
         ReceiptHandle=receipt_handle,
     )
 
+
 def receive_message():
-	message_body=''
-	response = sqs_client.receive_message(
-		QueueUrl='https://sqs.us-east-1.amazonaws.com/250982311839/project2_queue',
-		MaxNumberOfMessages=1,
-		WaitTimeSeconds=2,)
-	for message in response.get("Messages", []):
-		message_body = message["Body"]
-		delete_message(message["ReceiptHandle"])
-	return message_body
+    message_body = ''
+    response = sqs_client.receive_message(
+        QueueUrl='https://sqs.us-east-1.amazonaws.com/250982311839/project2_queue',
+        MaxNumberOfMessages=1,
+        WaitTimeSeconds=1, )
+    for message in response.get("Messages", []):
+        message_body = message["Body"]
+        delete_message(message["ReceiptHandle"])
+    return message_body
+
 
 print("Receiving SQS messages...")
 while True:
-	json_data = receive_message()
-	if not json_data == '':
-		#print(json_data)
-		i=i+1
-		y = json.loads(json_data)
-		print(f'The {i} person detected: {y["name"]}, {y["major"]}, {y["year"]}')
-		start_time = y["filename"][:-5]
-		#print(f'start_time: {start_time}, {type(start_time)}')
-		time_format = time.strptime(start_time, '%Y-%m-%d_%H.%M.%S.%f')
-		millis = float(f"0.{start_time.split('.')[-1]}")
-		start_timestamp = time.mktime(time_format)+millis
-		current_timestamp = time.time()
-		#print(start_time, start_timestamp, current_timestamp)
-		print(f'Latency: {"{:.2f}".format(current_timestamp-start_timestamp)} seconds.')
-		#print('\n')
-	else:
-		print("Empty Queue...")
+    json_data = receive_message()
+    if not json_data == '':
+        # print(json_data)
+        i = i + 1
+        y = json.loads(json_data)
+        print(f'The {i} person detected: {y["name"]}, {y["major"]}, {y["year"]}')
+        start_time = y["filename"][:-5]
+        # print(f'start_time: {start_time}, {type(start_time)}')
+        time_format = time.strptime(start_time, '%Y-%m-%d_%H.%M.%S.%f')
+        millis = float(f"0.{start_time.split('.')[-1]}")
+        start_timestamp = time.mktime(time_format) + millis
+        current_timestamp = time.time()
+        # print(start_time, start_timestamp, current_timestamp)
+        print(f'Latency: {"{:.2f}".format(current_timestamp - start_timestamp)} seconds.')
+    # print('\n')
+    else:
+        print("Empty Queue...")
